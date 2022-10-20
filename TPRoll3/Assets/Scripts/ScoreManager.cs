28bea410319d942cc72f939bdc909d08a3600ded
AUSTIN <58276668+6789123Hao@users.noreply.github.com>
2022-10-19 18:57:21 -0700

    added ScoreManager script with high score records

---
+++ TPRoll3/Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+//use updateScore to set highscore
+//use GetScore to retreive highscore
+
+public class ScoreManager : MonoBehaviour
+{
+    public Text scoreText;
+    private int highScore = 0;
+
+    private void Start ()
+    {
+        AddScore(0);
+    }
+
+    public void AddScore(int toAdd) {
+        highScore += toAdd;
+        scoreText.text = highScore.ToString();
+    }
+    public void UpdateScore(int newScore)
+    {
+        highScore = Mathf.Max(highScore, newScore);
+    }
+
+    public int GetScore()
+    {
+        return highScore;
+    }
+
+    public void WipeScore()
+    {
+        highScore= 0;
+    }
+
+}

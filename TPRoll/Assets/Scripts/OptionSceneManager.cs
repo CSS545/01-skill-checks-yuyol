2d2c85062145a1149df8cca8c1c2a054bcb1db9b
Yu Yang <ylctyuy@gmail.com>
2022-10-22 17:22:28 -0700

    change file name OptionSceneManager

--- TPRoll/Assets/Scripts/OptionSceneManager.cs
+++ TPRoll/Assets/Scripts/OptionSceneManager.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
-public class test1 : MonoBehaviour
+public class OptionSceneManager : MonoBehaviour
 {
     // Start is called before the first frame update
     // public Text text;

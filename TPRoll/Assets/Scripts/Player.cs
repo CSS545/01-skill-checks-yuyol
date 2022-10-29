6eb23c7e80d254d5fa840d81aa82e191812068b8
AUSTIN <58276668+6789123Hao@users.noreply.github.com>
2022-10-29 03:40:37 -0700

    Added Apple and speed up function

--- TPRoll/Assets/Scripts/Player.cs
+++ TPRoll/Assets/Scripts/Player.cs
@@ -4,8 +4,10 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    private int foodLimiter = 0;
     public MovementJoystick movementJoystick;
     public float playerSpeed;
+    public float logBase = 2;
     private Rigidbody2D rb;
 
     // Start is called before the first frame update
@@ -43,5 +45,18 @@ public class Player : MonoBehaviour
                 gameScreen.OnPlayerDeath();
             }
         }
+        if (collision.gameObject.CompareTag("Item"))
+        {
+            
+            Destroy(collision.gameObject);
+            //this go through all scene to find scene- bad, but ok for once
+
+            foodLimiter++;
+            playerSpeed = playerSpeed + Mathf.Log(playerSpeed, logBase) / foodLimiter;
+            Debug.Log(Mathf.Log(playerSpeed, logBase) + "/"+ foodLimiter + " = " + Mathf.Log(playerSpeed, logBase) / foodLimiter);
+            //7,2 15,0.28
+            //7,10 15,0.06
+        }
+
     }
 }
